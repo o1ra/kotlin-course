5f5311df609c1f7a4bd3348c125d887b709935bf
Irina Kirillova <95588789+o1ra@users.noreply.github.com>
2025-12-08 22:00:35 +0500

    homework24.kt

---
+++ src/main/kotlin/org/kotlintest/lessons/lesson25/homework/homework25.kt
@@ -0,0 +1,81 @@
+package org.example.org.kotlintest.lessons.lesson25.homework
+
+//Задание 1
+
+fun ex1(fnc: () -> Any): Long {
+    val start = System.currentTimeMillis()
+    fnc()
+    return System.currentTimeMillis() - start
+}
+
+fun main() {
+    val myFunction = {
+        val list = List(10000000) { (0..10000).random() }
+        list.sorted()
+    }
+    println(ex1(myFunction))
+}
+//Задание 2
+
+fun main1() {
+    val em = Employee("Ivan", 23, "QA Engineer").apply {
+        email = "ivan@gm.com"
+        department = "dep1"
+    }
+}
+//Задание 3
+
+fun printPerson(person: Person) {
+    with(person) {
+        println("""
+           Name: $name
+           Age: $age
+           Email: $email
+       """.trimIndent())
+    }
+}
+
+fun main2() {
+    val person = Person("Anna", 26).also {
+        it.email = "anna@gm.com"
+        printPerson(it)
+    }
+}
+//Задание 4
+
+fun main3() {
+    val person = Person("Andrew", 32).apply {
+        email = "andrew@gm.com"
+    }
+
+    val employee = with(person) {
+        Employee(name, age, "Front dev").also {
+            it.email = email
+            it.department = "dep2"
+        }
+    }
+}
+//Задание 5
+
+fun main4() {
+    val person = Person("Andrew", 32).apply {
+        email = "andrew@gm.com"
+    }
+
+    val employee = person.run {
+        Employee(name, age, "Front dev").also {
+            it.email = email
+            it.department = "dep2"
+        }
+    }
+}
+//Задание 6
+
+fun Person?.toEmployee(position: String, depart: String): Employee? {
+    return this?.let {
+        Employee(it.name, it.age, position).apply {
+            email = it.email
+            department = depart
+        }
+    }
+}
